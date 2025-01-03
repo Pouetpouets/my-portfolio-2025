09a707404681489a15db828170217fb8f7dc1a75
Lucas Legrand <legrand.lucas0@gmail.com>
2025-01-03 23:19:16 +0100

    Initial project setup with Vite and Svelte TypeScript

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  
+</script>
+
+<main>
+  <h1>My Portfolio 2025</h1>
+</main>
+
+<style>
+  main {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+  }
+</style>
